3440cef92da3272c3ad34283c23234e52d55ebba
mila-escolasorvete <126102649+mila-escolasorvete@users.noreply.github.com>
2023-02-27 18:48:21 -0300

    update baseurl to https

--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
 	process.env.NODE_ENV === "production"
-		? "http://develop.escolasorvete.com.br"
-		: "http://develop.escolasorvete.com.br";
+		? "https://develop.escolasorvete.com.br"
+		: "https://develop.escolasorvete.com.br";
 
 export default baseUrl;
