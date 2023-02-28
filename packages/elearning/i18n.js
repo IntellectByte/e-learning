17005cb748a6e6e34f85a938c21fba96025c2825
mila-escolasorvete <126102649+mila-escolasorvete@users.noreply.github.com>
2023-02-28 11:05:19 -0300

    update i18next

--- packages/elearning/i18n.js
+++ packages/elearning/i18n.js
@@ -1,26 +1,20 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      pt: {
-        translation: require("./locales/pt.json"),
-      },
-      es: {
-        translation: require("./locales/es.json"),
-      },
-      en: {
-        translation: require("./locales/en.json"),
-      },
-    },
-    fallbackLng: "pt", // si no se encuentra el idioma del navegador, utilizará portugués como idioma por defecto
+    fallbackLng: "pt",
     debug: true,
     interpolation: {
       escapeValue: false,
+    },
+    backend: {
+      loadPath: "locales/{{lng}}.json",
     },
   });
 
