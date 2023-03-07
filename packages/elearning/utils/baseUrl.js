a185747874e87e07ccc61d715e10af1935d17318
Juan Pedro Bastias <juan@escolasorvete.com.br>
2023-03-07 10:28:15 -0300

    working text pt home

--- packages/elearning/utils/baseUrl.js
+++
@@ -1,6 +0,0 @@
-const baseUrl =
-    process.env.NODE_ENV === 'production'
-        ? 'https://develop.escolasorvete.com.br'
-        : 'https://develop.escolasorvete.com.br';
-
-export default baseUrl;
