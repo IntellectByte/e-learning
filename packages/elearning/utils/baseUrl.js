a934ba3e5fda40e9c175ac0801b91040c47c6849
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-03-08 13:52:09 -0300

    Base Url - local

--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV === 'production'
-        ? 'https://escolasorvete-2ae03.web.app'
+        ? 'http://localhost:3000'
         : 'http://localhost:3000';
 
 export default baseUrl;
