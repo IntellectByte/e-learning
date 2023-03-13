d7b050e90e3232496b383f364d8bc4388265f6bb
nicolas-escolasorvete <126102976+nicolas-escolasorvete@users.noreply.github.com>
2023-03-13 10:20:19 -0300

    Update baseUrl.js

--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV === 'production'
-        ? 'http://localhost:3000'
-        : 'http://localhost:3000';
+        ? 'develop.escolasorvete.com.br'
+        : 'develop.escolasorvete.com.br';
 
 export default baseUrl;
