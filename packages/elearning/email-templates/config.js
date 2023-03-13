1fe10756a7eba9057afdf48c63c024770d489182
nicolas-escolasorvete <nicolas@escolasorvete.com.br>
2023-03-13 13:30:31 -0300

    mandrill integration

--- packages/elearning/email-templates/config.js
+++ packages/elearning/email-templates/config.js
@@ -1,20 +1,16 @@
 import nodemailer from "nodemailer";
 import mandrillTransport from 'nodemailer-mandrill-transport'
 
+
+const {MANDRILL_USER, MANDRILL_API_KEY, MANDRILL_HOST, MANDRILL_PORT} = process.env
 export const transport = nodemailer.createTransport({
 	// Yes. SMTP!
 	service: "SMTP",
-	host: "smtp.mandrillapp.com", // Amazon email SMTP hostname
+	host: MANDRILL_HOST, // Amazon email SMTP hostname
 	secureConnection: true, // use SSL
-	port: 587, // port for secure SMTP
+	port: MANDRILL_PORT, // port for secure SMTP
 	auth: {
-		user: 'Escola Sorvete', // Use from Amazon Credentials
-		pass: 'md-RG23QCjSJIi5Z_sN-01v_g', // Use from Amazon Credentials
+		user: MANDRILL_USER, // Use from Amazon Credentials
+		pass: MANDRILL_API_KEY, // Use from Amazon Credentials
 	},
 });
-
-// export const transport = nodemailer.createTransport(mandrillTransport({
-// 	auth: {
-// 		apiKey: 'md-RG23QCjSJIi5Z_sN-01v_g'
-// 	}
-// }));
