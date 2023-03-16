6dda094fc4960029896c49231dfcb6c3f3cc2753
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-03-16 11:43:51 -0300

    Fixed the pop up form and added functionality so when a user subscribe to it , in mailchimp we get the subscriber

--- packages/elearning/components/PopUp/PopUp.js
+++ packages/elearning/components/PopUp/PopUp.js
@@ -1,55 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { toast } from 'react-hot-toast';
 
 const PopUp = ({ onClose }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [emailValid, setEmailValid] = useState(true);
-
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
-
-    const handleEmailChange = (e) => {
-        const inputEmail = e.target.value;
-        setEmail(inputEmail);
-
-        // Valida que el formato de email sea correcto
-        const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-        setEmailValid(emailPattern.test(inputEmail));
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await fetch(
-                'https://escolasorvete.us17.list-manage.com/subscribe/post?u=861af04770adfc24846742c1c&amp;id=be824b0afb&amp;f_id=005521e0f0',
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        email_address: email,
-                        merge_fields: {
-                            FNAME: name,
-                        },
-                        status: 'subscribed',
-                    }),
-                }
-            );
-            if (response.ok) {
-                console.log('Email successfully submitted to Mailchimp');
-                onClose();
-                toast.success('Email submitted successfully!');
-            } else {
-                console.log('Failed to submit email to Mailchimp');
-            }
-        } catch (error) {
-            console.log('Error submitting email to Mailchimp', error);
-            toast.error('Failed to submit email. Please try again later.');
+    const handleSubmit = (e) => {
+        if (!e.target.checkValidity()) {
+            e.preventDefault();
+            toast.error('Please fill out all fields correctly.');
+            return;
         }
+        toast.success('Email submitted successfully!');
+        setTimeout(() => {
+            onClose();
+        }, 2000);
     };
 
     return (
@@ -69,35 +31,26 @@ const PopUp = ({ onClose }) => {
                         Subscreva a nossa newsletter e obtenha um código de{' '}
                         <span>desconto de 20%</span>
                     </p>
-                    <form onSubmit={handleSubmit}>
+                    <form
+                        onSubmit={handleSubmit}
+                        action='https://escolasorvete.us17.list-manage.com/subscribe/post'
+                        method='POST'
+                    >
+                        <input
+                            type='hidden'
+                            name='u'
+                            value='861af04770adfc24846742c1c'
+                        />
+                        <input type='hidden' name='id' value='be824b0afb' />
                         <label>
                             Digite seu nome:
-                            <input
-                                type='text'
-                                value={name}
-                                onChange={handleNameChange}
-                            />
+                            <input type='text' name='MERGE1' required />
                         </label>
                         <label>
                             Digite seu e-mail:
-                            <input
-                                type='email'
-                                value={email}
-                                onChange={handleEmailChange}
-                            />
+                            <input type='email' name='EMAIL' required />
                         </label>
-                        {!emailValid && (
-                            <p style={{ color: 'red' }}>
-                                Por favor insira um endereço de e-mail válido.
-                            </p>
-                        )}
-                        <button
-                            type='submit'
-                            onClick={onClose}
-                            disabled={!name || !email || !emailValid}
-                        >
-                            Quero o cupom
-                        </button>
+                        <button type='submit'>Quero o cupom</button>
                     </form>
                 </div>
             </div>
