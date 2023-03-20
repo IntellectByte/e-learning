7b06dc476d31f0b8eae6f69c0d7648d0b40a7656
Lucas Trigo <126102883+lucas-escolasorvete@users.noreply.github.com>
2023-03-20 11:14:22 -0300

    Update baseUrl.js

--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = process.env;
 const baseUrl =
     NODE_ENV === 'production'
-        ? 'http://localhost:3000'
+        ? 'https://develop.escolasorvete.com.br'
         : 'http://localhost:3000';
 
 export default baseUrl;
