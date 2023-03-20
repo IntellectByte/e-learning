c2a975d1d7c4d2fe3a1e96c18522eae9c37784a3
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-03-20 11:03:37 -0300

    Adding framer motion animations to index.js

--- packages/elearning/pages/index.js
+++ packages/elearning/pages/index.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+// ADDED ANIMATION LIBRARIES
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 import Navbar from '@/components/_App/Navbar';
 import MainBanner from '@/components/eLearningSchool/MainBanner';
 import Partner from '@/components/eLearningSchool/Partner';
@@ -14,8 +17,61 @@ import baseUrl from '@/utils/baseUrl';
 import PopUp from '@/components/PopUp/PopUp';
 import SupportButton from '@/components/ContactUs/SupportBtn';
 
+const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: { duration: 1.5, ease: 'easeInOut' },
+    },
+};
+
+const fadeInUp = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.2 },
+    },
+};
+
+const fadeInLeft = {
+    hidden: { opacity: 0, x: -20 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.4 },
+    },
+};
+
+const fadeInRight = {
+    hidden: { opacity: 0, x: 20 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.6 },
+    },
+};
+
 function Index({ courses, user }) {
     const [showPopup, setShowPopup] = useState(false);
+
+    // Animations on scroll
+    const [refMainBanner, inViewMainBanner] = useInView({
+        threshold: 0.9,
+        triggerOnce: true,
+    });
+    const [refFeatures, inViewFeatures] = useInView({
+        threshold: 0.9,
+        triggerOnce: true,
+    });
+    const [refAboutUs, inViewAboutUs] = useInView({
+        threshold: 0.9,
+        triggerOnce: true,
+    });
+    const [refPopularCourses, inViewPopularCourses] = useInView({
+        threshold: 0.9,
+        triggerOnce: true,
+    });
 
     useEffect(() => {
         const timeout = setTimeout(() => {
@@ -36,11 +92,41 @@ function Index({ courses, user }) {
         <>
             {showPopup && <PopUp onClose={handleClosePopup} />}
             <SupportButton />
-            <Navbar user={user} />
-            <MainBanner user={user} courses={courses} />
-            <Features />
-            <AboutUs />
-            <PopularCourses user={user} />
+            <motion.div initial='hidden' animate='visible' variants={fadeIn}>
+                <Navbar user={user} />
+            </motion.div>
+            <motion.div
+                ref={refMainBanner}
+                initial='hidden'
+                animate={inViewMainBanner ? 'visible' : 'hidden'}
+                variants={fadeInUp}
+            >
+                <MainBanner user={user} courses={courses} />
+            </motion.div>
+            <motion.div
+                ref={refFeatures}
+                initial='hidden'
+                animate={inViewFeatures ? 'visible' : 'hidden'}
+                variants={fadeInLeft}
+            >
+                <Features />
+            </motion.div>
+            <motion.div
+                ref={refAboutUs}
+                initial='hidden'
+                animate={inViewAboutUs ? 'visible' : 'hidden'}
+                variants={fadeInRight}
+            >
+                <AboutUs />
+            </motion.div>
+            <motion.div
+                ref={refPopularCourses}
+                initial='hidden'
+                animate={inViewPopularCourses ? 'visible' : 'hidden'}
+                variants={fadeInUp}
+            >
+                <PopularCourses user={user} />
+            </motion.div>
             <FeedbackSliderWithFunFacts />
             <GetInstantCourses user={user} />
             <ViewAllCourses />
--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,5 +1,7 @@
-const {NODE_ENV} = process.env
+const { NODE_ENV } = process.env;
 const baseUrl =
-    NODE_ENV === 'production' ? 'https://develop.escolasorvete.com.br' : 'http://localhost:3000'
+    NODE_ENV === 'production'
+        ? 'http://localhost:3000'
+        : 'http://localhost:3000';
 
 export default baseUrl;
