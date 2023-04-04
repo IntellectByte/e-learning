58b38293b202295c7b19ecfe6b26fee8b93dcb03
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-04-04 11:09:06 -0300

    Adjusting translations and payment status page

--- packages/elearning/pages/payment-status.js
+++ packages/elearning/pages/payment-status.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { useTranslation } from 'next-i18next';
 
 const iceCreams = [
     { id: 1, width: 50, duration: 8, left: '20%', top: '10%' },
@@ -21,48 +22,72 @@ const iceCreams = [
 ];
 
 const PaymentStatus = () => {
+    const { t } = useTranslation();
+    const [isMounted, setIsMounted] = React.useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
     return (
         <>
-            <div className='bluemoon'>
-                <div className='content-container'>
-                    <img src='/logo-horizontal.png' />
-                    <p class='title'>Gracias por tu compra!</p>
-                    <p class='subtitle'>
-                        En el mail te tiene que estar llegando la info del curso
-                        que compraste porfavor revisalo. Una vez que revisaste
-                        clickea el boton de abajo.
-                    </p>
-
-                    <div align='center'>
-                        <Link href='/learning/my-courses/'>
-                            <a class='btn-back' href='#'>
-                                Ir a mis cursos
-                            </a>
-                        </Link>
-                        <Link href='/checkout/'>
-                            <a class='btn-back' href='#'>
-                                Volver al checkout
-                            </a>
-                        </Link>
-
+            {isMounted && (
+                <div className='bluemoon'>
+                    <div className='content-container'>
+                        <img src='/logo-horizontal.png' />
+                        <p class='title'>
+                            {t('paymentStatusPage-Title', {
+                                defaultValue: 'Gracias por tu compra!',
+                            })}
+                        </p>
+                        <br />
+                        <p class='subtitle'>
+                            {t('paymentStatusPage-SubTitle1', {
+                                defaultValue:
+                                    'En el mail te tiene que estar llegando la info del curso que compraste porfavor revisalo.',
+                            })}
+                        </p>
+                        <p class='subtitle'>
+                            {t('paymentStatusPage-SubTitle2', {
+                                defaultValue:
+                                    'Para continuar con tu compra, porfavor revisa tu correo electronico.',
+                            })}
+                        </p>{' '}
+                        {/* Added text */}
+                        <div align='center'>
+                            <Link href='/learning/my-courses/'>
+                                <a class='btn-back' href='#'>
+                                    {t('paymentStatusPage-Button', {
+                                        defaultValue: 'Ir a mis cursos',
+                                    })}
+                                </a>
+                            </Link>
+                            <Link href='/checkout/'>
+                                <a class='btn-back' href='#'>
+                                    {t('paymentStatusPage-Button2', {
+                                        defaultValue: 'Volver al checkout',
+                                    })}
+                                </a>
+                            </Link>
+                        </div>
                     </div>
-                </div>
 
-                {iceCreams.map((iceCream) => (
-                    <img
-                        key={iceCream.id}
-                        src='https://media.discordapp.net/attachments/1076613189795586122/1083484472491655319/sorvete-logo.png'
-                        alt={`Ice Cream ${iceCream.id}`}
-                        className='falling-ice-cream'
-                        style={{
-                            width: iceCream.width,
-                            animationDuration: `${iceCream.duration}s`,
-                            left: iceCream.left,
-                            top: iceCream.top,
-                        }}
-                    />
-                ))}
-            </div>
+                    {iceCreams.map((iceCream) => (
+                        <img
+                            key={iceCream.id}
+                            src='https://media.discordapp.net/attachments/1076613189795586122/1083484472491655319/sorvete-logo.png'
+                            alt={`Ice Cream ${iceCream.id}`}
+                            className='falling-ice-cream'
+                            style={{
+                                width: iceCream.width,
+                                animationDuration: `${iceCream.duration}s`,
+                                left: iceCream.left,
+                                top: iceCream.top,
+                            }}
+                        />
+                    ))}
+                </div>
+            )}
         </>
     );
 };
