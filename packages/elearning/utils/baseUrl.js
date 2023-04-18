87adbc685bcb3cbd592846618eb93cdae72c983b
Nicolas <126102976+nicolas-escolasorvete@users.noreply.github.com>
2023-04-18 12:21:16 -0300

    Update baseUrl.js

--- packages/elearning/utils/baseUrl.js
+++ packages/elearning/utils/baseUrl.js
@@ -1,14 +1,7 @@
-const productionUrls = [
-  'https://www.escolasorvete.com.br',
-  'https://escolasorvete.com.br'
-];
-
 const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ? productionUrls[Math.floor(Math.random() * productionUrls.length)]
-    : process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://escolasorvete.com.br';
+    process.env.NODE_ENV === 'production'
+        ? 'https://escolasorvete.com.br'
+        : 'http://localhost:3000';
 
 export default baseUrl;
 
