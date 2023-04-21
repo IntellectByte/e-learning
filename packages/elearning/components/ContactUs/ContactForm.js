1f62016bf6ec04575e6c9fe1e742a8ac2ab15d53
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-04-21 11:25:17 -0300

    Modifying the entire contact us page, adding translations, adding interactive google maps with location to escola sorvete

--- packages/elearning/components/ContactUs/ContactForm.js
+++ packages/elearning/components/ContactUs/ContactForm.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import LoadingSpinner from '@/utils/LoadingSpinner';
 import baseUrl from '@/utils/baseUrl';
+import { useTranslation } from 'next-i18next';
 
 const INITIAL_STATE = {
     name: '',
@@ -13,10 +14,17 @@ const INITIAL_STATE = {
 };
 
 const ContactForm = () => {
+    const { t } = useTranslation();
+    const [isMounted, setIsMounted] = React.useState(false);
+
     const [contact, setContact] = useState(INITIAL_STATE);
     const [disabled, setDisabled] = React.useState(true);
     const [loading, setLoading] = React.useState(false);
     const [emailError, setEmailError] = useState('');
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
     React.useEffect(() => {
         const isContact = Object.values(contact).every((el) => Boolean(el));
@@ -87,91 +95,104 @@ const ContactForm = () => {
 
     return (
         <>
-            <div className='contact-form'>
-                <h2>Ready to Get Started?</h2>
-                <p>
-                    Your email address will not be published. Required fields
-                    are marked *
-                </p>
+            {isMounted && (
+                <div className='contact-form'>
+                    <h2>
+                        {t('contactform-text1', {
+                            defaultValue: 'Ready to Get Started?',
+                        })}
+                    </h2>
+                    <p>
+                        {t('contactform-text2', {
+                            defaultValue:
+                                'Your email address will not be published. Required fields are marked *',
+                        })}
+                    </p>
 
-                <form id='contactForm' onSubmit={handleSubmit}>
-                    <div className='row'>
-                        <div className='col-lg-6 col-md-6'>
-                            <div className='form-group'>
-                                <input
-                                    type='text'
-                                    placeholder='Your Name'
-                                    name='name'
-                                    value={contact.name}
-                                    onChange={handleChange}
-                                />
+                    <form id='contactForm' onSubmit={handleSubmit}>
+                        <div className='row'>
+                            <div className='col-lg-6 col-md-6'>
+                                <div className='form-group'>
+                                    <input
+                                        type='text'
+                                        placeholder='Seu nome'
+                                        name='name'
+                                        value={contact.name}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className='col-lg-6 col-md-6'>
+                                <div className='form-group'>
+                                    <input
+                                        type='text'
+                                        placeholder='Seu endereço de email'
+                                        name='email'
+                                        value={contact.email}
+                                        onChange={handleChange}
+                                    />
+                                    {emailError && (
+                                        <div className='error'>
+                                            {emailError}
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+
+                            <div className='col-lg-12 col-md-6'>
+                                <div className='form-group'>
+                                    <input
+                                        type='text'
+                                        placeholder='Seu número de telefone'
+                                        name='phone'
+                                        value={contact.phone}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className='col-lg-12 col-md-12'>
+                                <div className='form-group'>
+                                    <input
+                                        type='text'
+                                        name='subject'
+                                        value={contact.subject}
+                                        onChange={handleChange}
+                                        placeholder='Assunto'
+                                    />
+                                </div>
+                            </div>
+
+                            <div className='col-lg-12 col-md-12'>
+                                <div className='form-group'>
+                                    <textarea
+                                        cols='30'
+                                        rows='5'
+                                        placeholder='Escreva sua mensagem...'
+                                        name='message'
+                                        value={contact.message}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className='col-lg-12 col-sm-12 text-center'>
+                                <button
+                                    type='submit'
+                                    className='default-btn'
+                                    disabled={disabled}
+                                >
+                                    {t('contactform-text3', {
+                                        defaultValue: 'Send Message',
+                                    })}{' '}
+                                    {loading ? <LoadingSpinner /> : ''}
+                                </button>
                             </div>
                         </div>
-
-                        <div className='col-lg-6 col-md-6'>
-                            <div className='form-group'>
-                                <input
-                                    type='text'
-                                    placeholder='Your email address'
-                                    name='email'
-                                    value={contact.email}
-                                    onChange={handleChange}
-                                />
-                                {emailError && (
-                                    <div className='error'>{emailError}</div>
-                                )}
-                            </div>
-                        </div>
-
-                        <div className='col-lg-12 col-md-6'>
-                            <div className='form-group'>
-                                <input
-                                    type='text'
-                                    placeholder='Your phone number'
-                                    name='phone'
-                                    value={contact.phone}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-
-                        <div className='col-lg-12 col-md-12'>
-                            <div className='form-group'>
-                                <input
-                                    type='text'
-                                    name='subject'
-                                    value={contact.subject}
-                                    onChange={handleChange}
-                                    placeholder='Your Subject'
-                                />
-                            </div>
-                        </div>
-
-                        <div className='col-lg-12 col-md-12'>
-                            <div className='form-group'>
-                                <textarea
-                                    cols='30'
-                                    rows='5'
-                                    placeholder='Write your message...'
-                                    name='message'
-                                    value={contact.message}
-                                    onChange={handleChange}
-                                />
-                            </div>
-                        </div>
-
-                        <div className='col-lg-12 col-sm-12 text-center'>
-                            <button
-                                type='submit'
-                                className='default-btn'
-                                disabled={disabled}
-                            >
-                                Send Message {loading ? <LoadingSpinner /> : ''}
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+            )}
         </>
     );
 };
