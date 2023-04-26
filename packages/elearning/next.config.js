d8fe7d43a4b7b6ac4967d3633b4fbe26b1d914b9
Nicolas <126102976+nicolas-escolasorvete@users.noreply.github.com>
2023-04-26 17:20:21 -0300

    Update next.config.js

--- packages/elearning/next.config.js
+++ packages/elearning/next.config.js
@@ -16,11 +16,11 @@ const nextConfig = {
         GETNET_CHECKOUT_ENDPOINT: process.env.GETNET_CHECKOUT_ENDPOINT,
         BASIC_AUTH_GETNET: process.env.BASIC_AUTH_GETNET,
         JWT_SECRET: process.env.JWT_SECRET,
-        CLOUD_NAME: process.env.CLOUD_NAME,
-        // UPLOAD_PRESETS: process.env.UPLOAD_PRESETS,
-        // CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-        // CLOUDINARY_VIDEO_URL: process.env.CLOUDINARY_VIDEO_URL,
-        // CLOUDINARY_ZIP_URL: process.env.CLOUDINARY_ZIP_URL,
+        CLOUD_NAME: 'dnneps5fo',
+        UPLOAD_PRESETS: "images",
+        CLOUDINARY_URL: 'https://api.cloudinary.com/v1_1/dnunzwnex/image/upload',
+        CLOUDINARY_VIDEO_URL: 'https://api.cloudinary.com/v1_1/dnunzwnex/video/upload',
+        CLOUDINARY_ZIP_URL: 'https://api.cloudinary.com/v1_1/dnneps5f/raw/upload?api_key=284888778249526',
     },
     webpack: (config, { isServer }) => {
         config.module.rules.push({
