1cce72e890d1554d82ce9bb541ab0e94dabc64fa
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-04-27 11:38:56 -0300

    Translating course cards

--- packages/elearning/components/Courses/CourseCard.js
+++ packages/elearning/components/Courses/CourseCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import baseUrl from '@/utils/baseUrl';
+import { useTranslation } from 'next-i18next';
 
 const CourseCard = ({
     id,
@@ -20,11 +21,17 @@ const CourseCard = ({
 }) => {
     const [fav, setfavs] = useState(false);
     const [studentCount, setStudentCount] = useState(0);
+    const { t } = useTranslation();
+    const [isMounted, setIsMounted] = useState(false);
 
-  useEffect(() => {
-    const randomStudents = Math.floor(Math.random() * 100) + 300;
-    setStudentCount(enrolments.length + randomStudents);
-  }, [enrolments.length]);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
+        const randomStudents = Math.floor(Math.random() * 100) + 300;
+        setStudentCount(enrolments.length + randomStudents);
+    }, [enrolments.length]);
 
     useEffect(() => {
         if (userId) {
@@ -43,82 +50,92 @@ const CourseCard = ({
         }
     }, []);
     return (
-        <div className='col-lg-4 col-md-6'>
-            <div className='single-courses-box'>
-                <div className='courses-image'>
-                    <Link href={`/course/${slug}`}>
-                        <a className='d-block image'>
-                            <img src={image} alt={title} />
-                        </a>
-                    </Link>
+        <>
+            {isMounted && (
+                <div className='col-lg-4 col-md-6'>
+                    <div className='single-courses-box'>
+                        <div className='courses-image'>
+                            <Link href={`/course/${slug}`}>
+                                <a className='d-block image'>
+                                    <img src={image} alt={title} />
+                                </a>
+                            </Link>
 
-                    {fav ? (
-                        <button
-                            className='btn fav'
-                            onClick={() => {
-                                onUnFav(id);
-                                setfavs(!fav);
-                            }}
-                        >
-                            <i className='bx bxs-heart'></i>
-                        </button>
-                    ) : (
-                        <button
-                            className='btn fav'
-                            onClick={() => {
-                                onFav(id);
-                                setfavs(!fav);
-                            }}
-                        >
-                            <i className='bx bx-heart'></i>
-                        </button>
-                    )}
+                            {fav ? (
+                                <button
+                                    className='btn fav'
+                                    onClick={() => {
+                                        onUnFav(id);
+                                        setfavs(!fav);
+                                    }}
+                                >
+                                    <i className='bx bxs-heart'></i>
+                                </button>
+                            ) : (
+                                <button
+                                    className='btn fav'
+                                    onClick={() => {
+                                        onFav(id);
+                                        setfavs(!fav);
+                                    }}
+                                >
+                                    <i className='bx bx-heart'></i>
+                                </button>
+                            )}
 
-                    <>
-                        {before_price > 0 && (
-                            <div className='price shadow discount-price'>
-                                <del>${before_price}</del>
-                            </div>
-                        )}
-                        <div className='price shadow'>
-                            <span>${latest_price}</span>
+                            <>
+                                {before_price > 0 && (
+                                    <div className='price shadow discount-price'>
+                                        <del>${before_price}</del>
+                                    </div>
+                                )}
+                                <div className='price shadow'>
+                                    <span>${latest_price}</span>
+                                </div>
+                            </>
                         </div>
-                    </>
-                </div>
-                <div className='courses-content'>
-                    <div className='course-author d-flex align-items-center'>
-                        <img
-                            src={
-                                user.profile_photo
-                                    ? user.profile_photo
-                                    : '/images/user6.jpg'
-                            }
-                            className='rounded-circle'
-                            alt='image'
-                        />
-                        <span>{`${user.first_name} ${user.last_name}`}</span>
+                        <div className='courses-content'>
+                            <div className='course-author d-flex align-items-center'>
+                                <img
+                                    src={
+                                        user.profile_photo
+                                            ? user.profile_photo
+                                            : '/images/user6.jpg'
+                                    }
+                                    className='rounded-circle'
+                                    alt='image'
+                                />
+                                <span>{`${user.first_name} ${user.last_name}`}</span>
+                            </div>
+
+                            <h3>
+                                <Link href={`/course/${slug}`}>
+                                    <a title={title}>{title.slice(0, 40)}...</a>
+                                </Link>
+                            </h3>
+
+                            <p>{short_desc.slice(0, 108)}</p>
+                            <ul className='courses-box-footer d-flex justify-content-between align-items-center'>
+                                <li>
+                                    <i className='flaticon-agenda'></i>{' '}
+                                    {lessons}{' '}
+                                    {t('coursecard-lessons', {
+                                        defaultValue: 'Lessons',
+                                    })}
+                                </li>
+                                <li>
+                                    <i className='flaticon-people'></i>{' '}
+                                    {studentCount}{' '}
+                                    {t('coursecard-students', {
+                                        defaultValue: 'Students',
+                                    })}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-
-                    <h3>
-                        <Link href={`/course/${slug}`}>
-                            <a title={title}>{title.slice(0, 40)}...</a>
-                        </Link>
-                    </h3>
-
-                    <p>{short_desc.slice(0, 108)}</p>
-                    <ul className='courses-box-footer d-flex justify-content-between align-items-center'>
-                        <li>
-                            <i className='flaticon-agenda'></i> {lessons}{' '}
-                            Lessons
-                        </li>
-                        <li>
-                            <i className='flaticon-people'></i>{' '}
-                            {studentCount} Students
-                        </li>
-                    </ul>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
