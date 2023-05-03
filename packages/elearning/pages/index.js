5540ea5d3ed71922743a605e79446fb44569f890
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-05-03 16:28:48 -0300

    Modifying index

--- packages/elearning/pages/index.js
+++ packages/elearning/pages/index.js
@@ -1,12 +1,12 @@
+// EXTERNAL IMPORTS
 import React, { useState, useEffect } from 'react';
-// ADDED ANIMATION LIBRARIES
+
+// INTERNAL IMPORTS
 import Navbar from '@/components/_App/Navbar';
 import MainBanner from '@/components/eLearningSchool/MainBanner';
-import Partner from '@/components/eLearningSchool/Partner';
 import Features from '@/components/eLearningSchool/Features';
 import AboutUs from '@/components/eLearningSchool/AboutUs';
 import PopularCourses from '@/components/eLearningSchool/PopularCourses';
-// import FeedbackSliderWithFunFacts from '@/components/eLearningSchool/FeedbackSliderWithFunFacts';
 import GetInstantCourses from '@/components/eLearningSchool/GetInstantCourses';
 import ViewAllCourses from '@/components/eLearningSchool/ViewAllCourses';
 import SubscribeForm from '@/components/Common/SubscribeForm';
@@ -16,42 +16,6 @@ import PopUp from '@/components/PopUp/PopUp';
 import SupportButton from '@/components/ContactUs/SupportBtn';
 import TopBanner from '@/components/TopBanner/TopBanner';
 import FunFacts from '@/components/eLearningSchool/FunFacts';
-// import StudentReviewSlider from '@/components/StudentsReviewSlider/StudentReviewSlider';
-
-const fadeIn = {
-    hidden: { opacity: 0 },
-    visible: {
-        opacity: 1,
-        transition: { duration: 1.5, ease: 'easeInOut' },
-    },
-};
-
-const fadeInUp = {
-    hidden: { opacity: 0, y: 20 },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.2 },
-    },
-};
-
-const fadeInLeft = {
-    hidden: { opacity: 0, x: -20 },
-    visible: {
-        opacity: 1,
-        x: 0,
-        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.4 },
-    },
-};
-
-const fadeInRight = {
-    hidden: { opacity: 0, x: 20 },
-    visible: {
-        opacity: 1,
-        x: 0,
-        transition: { duration: 1.5, ease: 'easeInOut', delay: 0.6 },
-    },
-};
 
 function Index({ courses, user }) {
     const [showPopup, setShowPopup] = useState(false);
@@ -62,7 +26,7 @@ function Index({ courses, user }) {
                 setShowPopup(true);
                 sessionStorage.setItem('popupShown', true);
             }
-        }, 30000); // Added feature to load after 30 seconds of being on the website.
+        }, 30000);
 
         return () => clearTimeout(timeout);
     }, []);
