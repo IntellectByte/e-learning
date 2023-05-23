232db1bc9571540f77d19f0d3848e6e130498835
Juan Pedro Bastias <juan@escolasorvete.com.br>
2023-05-23 10:34:14 -0300

    add

--- packages/elearning/components/eLearningSchool/MainBanner.js
+++ packages/elearning/components/eLearningSchool/MainBanner.js
@@ -1,125 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
-/* import BannerCourses from './BannerCourses'; */
 import { useTranslation } from "next-i18next";
-import { useSelector, useDispatch } from "react-redux";
-import { Pricing } from "react-pricing";
-import styles from "../../components/SubscribePlan/SubscribePlanComponent.module.css";
-import { useRouter } from "next/router";
-import axios from "axios";
-import baseUrl from "@/utils/baseUrl";
+import SubsHome from "../SubsHome/SubsHome";
 
 const MainBanner = ({ user, courses }) => {
   const { t } = useTranslation();
   const [isMounted, setIsMounted] = useState(false);
 
-  const dispatch = useDispatch();
-  const cartItems = useSelector((state) => state.cart.cartItems);
-  const router = useRouter();
-  const [subs, setSubs] = useState(null);
-
-  let [yr, setYr] = useState({
-    xpathResult1: "",
-  });
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  function fetchData() {
-    axios
-      .get(`${baseUrl}/api/subscriptions`)
-      .then((res) => {
-        setSubs(res.data);
-        // console.log(subs)
-      })
-      .catch((err) => console.error("Error fetching data:", err));
-  }
-
-  useEffect(() => {
-    fetchData();
-    // Clean up function (optional)
-    return () => {
-      setSubs(null);
-    };
-  }, []);
-
-  useEffect(() => {
-    setIsMounted(true);
-
-    setTimeout(() => {
-      getElementsWithXPath();
-    }, 1200);
-    // Check if the element was found
-
-    setTimeout(() => {
-      if (yr.xpathResult1.singleNodeValue) {
-        // Access the element
-        const element = yr.xpathResult1.singleNodeValue;
-        const spans = Array.from(element.querySelectorAll("div span")).map(
-          (e) => {
-            if (e.innerHTML === " / yr") {
-              return e;
-            }
-          }
-        );
-
-        for (const span of spans) {
-          if (span) {
-            span.innerHTML = "/ano";
-          }
-        }
-      } else {
-        console.log("Element not found.");
-      }
-    }, 1600);
-  }, []);
-
-  function getElementsWithXPath() {
-    yr = {
-      xpathResult1: document.evaluate(
-        '//*[@id="__next"]/div[4]/div/div',
-        document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-      ),
-    };
-  }
-
-  const subscribePlan1 = (sub) => {
-    console.log(sub);
-
-    const planObj = {
-      id: sub.id,
-      title: sub.title,
-      slug: sub.slug,
-      price: sub.price,
-      image: sub.image,
-      duration: sub.duration,
-      quantity: 1,
-      hotmartId: "bwwp449q",
-      instructor: sub.instructor,
-      discount: 0,
-      type: sub.type,
-    };
-
-    dispatch({
-      type: "RESET_CART",
-    });
-
-    dispatch({
-      type: "ADD_TO_CART",
-      data: planObj,
-    });
-
-    router.push("/checkout");
-  };
-
-  const alreadySubscribe = cartItems.some((item) => {
-    return item.slug === "subscription-plan";
-  });
 
   return (
     <>
@@ -166,85 +57,29 @@ const MainBanner = ({ user, courses }) => {
                   </motion.div>
                 </div>
               </div>
-              <div className="col-lg-6 col-md-12">
-                <div className="d-flex">
-                  <div className="col-md-6">
-                    <div>
-                      <div className={styles.wrapper}>
-                        <h2>
-                          {/*  {t('subcomponent-h2', {
-              defaultValue: 'Subscribe to our plans',
-            })} */}
-                          <br />
-                        </h2>
-                        <br />
 
-                        {subs && (
-                          <div className={styles["pricing-table"]} style={{flexWrap:"nowrap"}}>
-                            {subs.slice(0, 2).map((sub) => (
-                              <Pricing
-                                id="custom-pricing-component"
-                                data={sub.perks}
-                                price={sub.price}
-                                duration="ano"
-                                background={sub.background}
-                                shadow="#F5BCD7"
-                                currency="R$"
-                                buttonContent={
-                                  alreadySubscribe ? (
-                                    <a onClick={() => router.push("/checkout")}>
-                                      {t("subcomponent-already-subscribe", {
-                                        defaultValue: "View My Subscription",
-                                      })}
-                                    </a>
-                                  ) : (
-                                    <a onClick={() => subscribePlan1(sub)}>
-                                      {t("subcomponent-subscribe-btn", {
-                                        defaultValue: "INSCREVER",
-                                      })}
-                                    </a>
-                                  )
-                                }
-                                priceText="Até 10x"
-                                headerText={sub.slug}
-                              />
-                            ))}
-                          </div>
-                        )}
-                      </div>
-                    </div>
+              <div className="col-lg-6 col-md-12">
+                <div className="main-banner-courses-list">
+                  <div className="row">
+                    <SubsHome />
+                  </div>
+
+                  <div className="banner-shape1">
+                    <img
+                      src="/sorvete-logo.png"
+                      alt="image"
+                      className="navbar-logo shape-moving"
+                    />
+                  </div>
+                  <div className="banner-shape2">
+                    <img
+                      src="/sorvete-logo.png"
+                      alt="image"
+                      className="navbar-logo shape-moving"
+                    />
                   </div>
                 </div>
               </div>
-
-              {/* <div className='col-lg-6 col-md-12'>
-                                <div className='main-banner-courses-list'>
-                                    <div className='row'>
-                                        {courses &&
-                                            courses.map((course) => (
-                                                <BannerCourses
-                                                    key={course.id}
-                                                    {...course}
-                                                />
-                                            ))}
-                                    </div>
-
-                                    <div className='banner-shape1'>
-                                        <img
-                                            src='/sorvete-logo.png'
-                                            alt='image'
-                                            className='navbar-logo shape-moving'
-                                        />
-                                    </div>
-                                    <div className='banner-shape2'>
-                                        <img
-                                            src='/sorvete-logo.png'
-                                            alt='image'
-                                            className='navbar-logo shape-moving'
-                                        />
-                                    </div>
-                                </div>
-                            </div> */}
             </div>
           </div>
         </div>
