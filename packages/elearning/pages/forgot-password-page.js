60709e2bc1ab6eb1dddbbbbc2d8df8c0acbb8dbb
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-06-12 15:37:07 -0300

    Adding translations

--- packages/elearning/pages/forgot-password-page.js
+++ packages/elearning/pages/forgot-password-page.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from '@/components/_App/Navbar';
-import PageBanner from '@/components/Common/PageBanner';
 import ForgotPassComponent from '@/components/Authentication/ForgotPassComponent';
 import Footer from '@/components/_App/Footer';
 import SupportButton from '@/components/ContactUs/SupportBtn';
@@ -16,16 +15,7 @@ export default function ForgotPasswordPage({ user }) {
 
             <Navbar user={user} />
 
-            <PageBanner
-                pageTitle='Forgot Password'
-                homePageUrl='/'
-                homePageText='Home'
-                activePageText='Forgot Password'
-            />
-
             <ForgotPassComponent />
-
-            <SubscribeForm />
 
             <Footer />
         </>
