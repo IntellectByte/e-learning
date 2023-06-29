7627061e39ef1fbde9b397a3299590b12fb074a3
lucas-escolasorvete <lucas@escolasorvete.com.br>
2023-06-29 15:33:09 -0300

    Notifications implement

--- packages/elearning/components/_App/NotificationModal.js
+++ packages/elearning/components/_App/NotificationModal.js
@@ -3,15 +3,49 @@ import React from 'react';
 const NotificationModal = ({ onClose, notifications }) => {
     return (
         <div className='notification-modal'>
-            <h3>Notifications</h3>
-            <ul>
+            <button
+                onClick={onClose}
+                style={{
+                    position: 'absolute',
+                    top: '8px',
+                    right: '8px',
+                    background: 'none',
+                    border: 'none',
+                    padding: '0',
+                    cursor: 'pointer',
+                    color: '#888888',
+                    fontSize: '20px',
+                }}
+            >
+                X
+            </button>
+            <h3
+                style={{
+                    textAlign: 'center',
+                    textDecoration: 'underline',
+                    color: '#CD4B85',
+                    fontWeight: 'bold',
+                }}
+            >
+                Notifications
+            </h3>
+            <ul className='notification-list'>
                 {notifications.map((notification) => (
-                    <li key={notification.id}>
-                        <a href={notification.link}>{notification.message}</a>
+                    <li key={notification.id} className='notification-item'>
+                        <a
+                            href={notification.link}
+                            style={{
+                                color: '#333333',
+                                textDecoration: 'none',
+                                fontWeight: 'bold',
+                                marginLeft: '10px',
+                            }}
+                        >
+                            {notification.message}
+                        </a>
                     </li>
                 ))}
             </ul>
-            <button onClick={onClose}>Close</button>
         </div>
     );
 };
